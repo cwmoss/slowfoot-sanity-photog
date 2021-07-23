eca7d24d984b5100856c827cedfd24d70c1be78d
Robert Wagner <rw@20sec.net>
2021-07-23 21:05:23 +0200

    first version

--- web/config.php
+++ web/config.php
@@ -1,26 +1,20 @@
 <?php
 
 return [
-    'site_name' => 'mumok Demo',
+    'site_name' => 'Photographers Protfolio',
     'site_description' => 'look at beautiful works of art',
     'site_url' => '',
     // TODO: solve genenv vs ENV problem
     'path_prefix' => getenv('PATH_PREFIX') ?: $_ENV['PATH_PREFIX'] ?: '',
     'title_template' => '',
     'sources' => [
-        /*'dataset' => 'dataset-mumok.ndjson', */
         'sanity' => [
             'dataset' => 'production',
             'projectId' => $_ENV['SANITY_ID'],
             'useCdn' => true,
             // 'query' => '*[_type=="custom-type-query"]'
         ]
-        /*
-        'csv' => [
-            'file' => '../movie-dataset-archive/movies_metadata.csv',
-            'jsol' => true,
-            'type' => 'movie'
-        ]*/
+       
     ],
     'preview' => [
         'sanity' => [
@@ -32,21 +26,7 @@ return [
         ]
     ],
     'templates' => [
-        'artist' => function ($obj) {
-            return '/artist/' . URLify::filter($obj['firstname'] . ' ' . $obj['familyname'], 60, 'de');
-        },
-        'work' => [
-            '/works/:_id',
-            [
-                'name' => 'en',
-                'path' => '/works/:_id/en'
-            ]
-        ],
-        'tag' => '/tag/:name',
-        'newsletter' => '/newsletter/:slug.current',
         'gallery_page' => '/g/:slug.current',
-        'movie' => '/movie/:_id'
-        //fn ($doc) => 'newsletter/' . $doc['slug']['current']
     ],
     'assets' => [
         'dir' => 'images',
@@ -58,48 +38,13 @@ return [
             ],
             'gallery' => [
                 's' => '700x', 
-                '4c' => ['creator'=>'Robbie Øfchen']
+                '4c' => ['creator'=>'Artist Name here :)']
             ]
         ]
     ],
     'hooks' => [
         'on_load' => function ($row, $ds) {
-            // [_id] => a-_a:325
-            if (preg_match('/:/', $row['_id'])) {
-                return null;
-            }
-            /*
-                works.tags should become a object of type 'tag'
-                make reverse reference from tag to works via tag.works
-                this will be faster to query for tag pages
-            */
-            if ($row['tags']) {
-                $tags = split_tags($row['tags']);
-                $refs = [];
-                foreach ($tags as $t) {
-                    $name = URLify::filter($t, 60, 'de');
-                    $title = $t;
-                    $id = 't-' . $name;
-                    $exists = $ds->get($id);
-                    if ($exists) {
-                        $ds->add_ref($id, 'works', $row);
-                    } else {
-                        $ds->add(
-                            $id,
-                            [
-                                '_type' => 'tag',
-                                'name' => $name,
-                                'title' => $t,
-                                'works' => [
-                                    ['_ref' => $row['_id']]
-                                ]
-                            ]
-                        );
-                    }
-                    $refs[] = ['_ref' => $id];
-                }
-                $row['tags'] = $refs;
-            }
+            // nothing to do
             return $row;
         }
     ]
